a282e1251afd98baa69c128b8d4fc2adc5779663
Brian Zakariya <zakariyabrian@gmail.com>
2025-05-02 22:07:04 +0700

    Bikin bagian modelnya

--- lvl4-todo-list/server/models/Todo.js
+++ lvl4-todo-list/server/models/Todo.js
@@ -1,21 +1,26 @@
-// models/Todo
-
 const mongoose = require("mongoose");
 
-const TodoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  priority: {
-    type: String,
-    enum: ["low", "medium", "high"],
-    default: "medium",
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
   },
-  status: {
-    type: String,
-    enum: ["pending", "in-progress", "completed"],
-    default: "pending",
-  },
-  createdAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Todo", TodoSchema);
+module.exports = mongoose.model("Task", taskSchema);
